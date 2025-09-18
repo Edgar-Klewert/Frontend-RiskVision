9084eb3f16409d61264ce32779d63f297521c44a
Adler Castro <adlercastro01@gmail.com>
2025-09-17 21:02:42 -0300

    :sparkles: feat: add enums for page routes and authentication states

---
+++ src/enums/pages.enum.ts
@@ -0,0 +1,18 @@
+export enum PAGES {
+  INITIAL = '/',
+  REGISTER = '/register',
+  LOGIN = '/login',
+  FORGOT_PASSWORD = '/forgot-password',
+  HOME = '/home',
+}
+
+export enum NO_AUTH {
+  INITIAL = '/',
+  REGISTER = '/register',
+  LOGIN = '/login',
+  FORGOT_PASSWORD = '/forgot-password',
+}
+
+export enum NEED_AUTH {
+  HOME = '/home',
+}
