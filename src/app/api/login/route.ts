9a70aeed7854f30569f307e106e0da1ec36cb2e8
Adler Castro <adlercastro01@gmail.com>
2025-09-22 16:51:23 -0300

    :sparkles: feat: enhance login response with success status and user data

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,6 +5,11 @@ import { loginSchema } from '@/schemas/login.schema';
 export async function POST(request: Request) {
   const data = await request.json();
   const parsedData = loginSchema.parse(data);
+  const { password, ...safe } = parsedData;
 
-  return new Response(JSON.stringify(parsedData), { status: 200 });
+  return new Response(JSON.stringify({ success: true, data: safe }), {
+    status: 200,
+    statusText: `Bem-vindo de volta, ${safe}`,
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
