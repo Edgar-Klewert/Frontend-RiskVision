4b5a1fb5036269dacaf358647205a416386e4767
Adler Castro <adlercastro01@gmail.com>
2025-09-22 16:52:09 -0300

    :label: feat: add registration schema with validation for user input

---
+++ src/schemas/register.schema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(3, 'Name is required'),
+    surname: z.string().min(3, 'Surname is required'),
+    email: z.email('Invalid email').min(1, 'Email is required'),
+    password: z.string().min(6, 'Invalid password'),
+    confirmPassword: z.string().min(6, 'Invalid password'),
+  })
+  .superRefine((val, ctx) => {
+    if (val.password !== val.confirmPassword) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Passwords do not match',
+        path: ['confirmPassword'],
+      });
+    }
+  });
+
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
