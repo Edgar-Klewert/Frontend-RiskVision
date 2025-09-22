86cef2b1e2e6ef36007667ce08d12def25f73cff
Adler Castro <adlercastro01@gmail.com>
2025-09-22 17:09:25 -0300

    refactor: reorganize tasks in the Home component and update wireframe details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,48 @@ export default function Home() {
           <li>
             Implementar funções genéricas para a requisição de esqueceu a senha
           </li>
-          <li>Página de Registro &#40; sprint 2 &#41;</li>
           <li>
-            Implementar funções genéricas para a requisição de cadastro &#40;
-            sprint 2 &#41;
+            Wireframe da Landing Page &#40; Página inicial com informações sobre
+            o sistema &#41;
           </li>
+          <li>Landing Page (Página inicial com informações sobre o sistema)</li>
         </ul>
 
         <h2 className='text-2xl font-bold'>Done:</h2>
-        <ul className='list-disc'>
-          <li>
-            <Link href={`${PAGES.LOGIN}`}>Página de Login</Link>
-          </li>
-          <li>Implementar funções genéricas para a requisição de login</li>
-        </ul>
+        <div className='flex gap-3'>
+          <div className='flex max-w-sm flex-col gap-2 text-balance'>
+            <h3 className='text-lg font-bold'>
+              Sprint 1 - Configurações Iniciais e Design das Páginas Básicas
+            </h3>
+            <ul className='list-disc'>
+              <li>
+                Wireframe das páginas de Login, Registro e Esqueceu a Senha
+              </li>
+              <li>
+                Configurações iniciais do projeto &#40;definição de estilos
+                customizáveis, componentes, CI/CD, entre outras configs.&#41;
+              </li>
+              <li>
+                <Link href={`${PAGES.LOGIN}`}>Página de Login</Link>
+              </li>
+              <li>Implementar funções genéricas para a requisição de login</li>
+            </ul>
+          </div>
+          <div className='flex max-w-sm flex-col gap-2 text-balance'>
+            <h3 className='text-lg font-bold'>
+              Sprint 2 - Autenticação de Usuário e Páginas de Registro e
+              Esqueceu a Senha
+            </h3>
+            <ul className='list-disc'>
+              <li>
+                <Link href={`${PAGES.REGISTER}`}>Página de Registro</Link>
+              </li>
+              <li>
+                Implementar funções genéricas para a requisição de cadastro
+              </li>
+            </ul>
+          </div>
+        </div>
       </Container>
     </div>
   );
