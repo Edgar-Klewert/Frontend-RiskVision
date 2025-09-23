7be69d33d7c08ae32d5134234188f9c2f2ade342
Adler Castro <adlercastro01@gmail.com>
2025-09-23 11:55:06 -0300

    :recycle: refactor: update RegisterAction to use environment variable for API URL and improve response handling

--- src/actions/register.actions.ts
+++ src/actions/register.actions.ts
@@ -1,40 +1,51 @@
 'use server';
 
+import { env } from '@/lib/env';
 import { RegisterSchemaType } from '@/schemas/register.schema';
 
-interface RegisterResponse {
-  success: boolean;
-  data: {
-    name: string;
-    surname: string;
-    email: string;
-  };
+interface RegisterDataResponse {
+  name: string;
+  surname: string;
+  email: string;
+  created_at: string;
+}
+
+interface RegisterResponse extends RegisterDataResponse {
+  detail?: string;
 }
 
 export async function RegisterAction(
   data: RegisterSchemaType,
 ): Promise<{ success: boolean; message: string }> {
   try {
-    const response = await fetch('http://localhost:3000/api/register', {
+    const nameComplete = `${data.name} ${data.surname}`.trim();
+    const dataFilter = {
+      name: nameComplete,
+      email: data.email,
+      password: data.password,
+      role_id: 2, // Usuário padrão
+    };
+
+    const response = await fetch(`${env.API_URL}/users`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
+      body: JSON.stringify(dataFilter),
       credentials: 'include',
     });
 
     const responseData: RegisterResponse = await response.json();
 
-    if (response.status !== 201) {
+    if (response.status !== 200) {
       return {
         success: false,
-        message: `Erro ao fazer cadastro. ${response.statusText || 'Erro desconhecido'}`,
+        message: `Erro ao fazer cadastro. ${responseData.detail || 'Erro desconhecido'} \n Status: ${response.status} `,
       };
     }
 
     return {
       success: true,
       message:
-        `Cadastro de ${responseData.data.name} ${responseData.data.surname} realizado com sucesso.` ||
+        `Cadastro de ${responseData.name} realizado com sucesso.` ||
         'Cadastro realizado com sucesso.',
     };
   } catch (error) {
