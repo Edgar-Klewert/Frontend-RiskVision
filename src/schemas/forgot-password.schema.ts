58862af417dab18085863525b758cfd96c94b2c2
AdlerCastro <adlercastro01@gmail.com>
2025-10-01 22:14:27 -0300

    :sparkles: feat: enhance password validation in ForgotPassword and Register schemas

--- src/schemas/forgot-password.schema.ts
+++ src/schemas/forgot-password.schema.ts
@@ -3,7 +3,12 @@ import { z } from 'zod';
 export const forgotPasswordSchema = z
   .object({
     email: z.email('Invalid email').min(1, 'Email is required'),
-    password: z.string().min(6, 'Invalid password'),
+    password: z
+      .string()
+      .min(6, 'Password must have at least 6 characters')
+      .regex(/[a-z]/, 'Password must contain at least 1 lowercase letter')
+      .regex(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
+      .regex(/[0-9]/, 'Password must contain at least 1 number'),
     confirmPassword: z.string().min(6, 'Invalid password'),
   })
   .superRefine((val, ctx) => {
--- src/schemas/register.schema.ts
+++ src/schemas/register.schema.ts
@@ -5,7 +5,12 @@ export const registerSchema = z
     name: z.string().min(3, 'Name is required'),
     surname: z.string().min(3, 'Surname is required'),
     email: z.email('Invalid email').min(1, 'Email is required'),
-    password: z.string().min(6, 'Invalid password'),
+    password: z
+      .string()
+      .min(6, 'Password must have at least 6 characters')
+      .regex(/[a-z]/, 'Password must contain at least 1 lowercase letter')
+      .regex(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
+      .regex(/[0-9]/, 'Password must contain at least 1 number'),
     confirmPassword: z.string().min(6, 'Invalid password'),
   })
   .superRefine((val, ctx) => {
